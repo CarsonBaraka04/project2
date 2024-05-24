ccbc12c70cec94d8ad62ef5d55b0e520a29b8a89
CarsonBaraka04 <barakacarson@gmail.com>
2024-05-24 11:37:33 +0300

    added a do-while loop

--- loops.php
+++ loops.php
@@ -7,8 +7,17 @@ while($in < 10){
     print $in . "<br>";
     $in++;
 }
+?>
 
-
+<h4>do-while-loop</h4>
+<?php
+//do-while loop
+$s = 5;
+do{
+    print $s . "<br>";
+    $s++;
+}while($s < 15);
+?>
 
 
 
