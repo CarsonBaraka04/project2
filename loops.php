2f6d74205c4a4791a108e6954822ceedbc609446
CarsonBaraka04 <barakacarson@gmail.com>
2024-05-24 11:44:27 +0300

    addition of for-loop

--- loops.php
+++ loops.php
@@ -11,16 +11,26 @@ while($in < 10){
 
 <h4>do-while-loop</h4>
 <?php
+
 //do-while loop
-$s = 5;
-do{
-    print $s . "<br>";
-    $s++;
-}while($s < 15);
+    $s = 5;
+    do{
+        print $s . "<br>";
+        $s++;
+    }while($s < 15);
+    
 ?>
 
 
+<h4>For loop</h4>
+<?php
 
+    //for loop
+    for($f=45; $f<55; $f++){
+        print $f . "<br>";
+    }
+
+?>
 
 
 
