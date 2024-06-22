d24fc7b1872a9a11b7e3bb893c583a52a65d5826
CarsonBaraka04 <barakacarson@gmail.com>
2024-06-22 13:43:11 +0300

    changed html to php

--- contacts.php
+++ contacts.php
@@ -7,19 +7,7 @@
 
     </head>
     <body>
-
-<div class="toplink">
-    <a href="./">Home</a>
-    <a href="about.html">About Us</a>
-    <a href="projects.html">Our Projects</a>
-    <a href="products.html">Our Products</a>
-    <a href="contacts.html">Contact Us</a>
-
-    <div class="toplink-right">
-        <a href="signup.html">Sign Up</a>
-        <a href="signin.html">Sign In</a>
-    </div>
-</div>
+        <?php include_once ("templates/nav.php"); ?>    
 
 <div class="banner">
     <h1>Banner</h1> 
