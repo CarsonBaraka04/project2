ce8a46fae2a69fef9b7ca45dfc6a64103493cec2
CarsonBaraka04 <barakacarson@gmail.com>
2024-07-11 12:21:45 +0300

    fixed an input bug

--- signin.php
+++ signin.php
@@ -1,25 +1,17 @@
-<?php
-require 'includes/db_connect.php';
 
-if(isset($_POST["submit"])){
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-
-    $gender = $_POST["age"];
-    $gender = "";
-    foreach($gender as $row){
-        $gender .= $row . ",";
-    }
-
-    $query = "INSERT INTO element VALUES('', '$name', '$gender', '$email', '$password')";
-    mysqli_query($conn, $query);
-    echo "<script> alert('Data Inserted Succecfully'); </script>";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+<body>
     
-}
-?>
 
-<?php include_once ("templates/heading.php"); ?>
+</html>
 <?php require_once ("templates/nav.php"); ?>
 <style media="screen">
     label{
@@ -30,35 +22,61 @@ if(isset($_POST["submit"])){
         <div class="banner">
             <h1>Sign In to TEMA library</h1> 
         </div>
-        <div class="row">
-            
-                <form action="" action="" method="" autocomplete="off">
-                    <label for="Name">Name</label>
-                    <input type="text" name="name" required value="">
-                    <br><br>
+        <div class="containerform">
+  <form method="post">
+    <div class="mb-3">
+      <label for="name" class="form-label">Name</label>
+      <input name="name" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+      <label for="username" class="form-label">Username</label>
+      <input name="username" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+      
+    </div>
+    <button type="submit"  name="submit" class="btn btn-primary">Sign in</button>
+    <a href="signin.php">Sign Up</a>
 
-                    <select name="Gender" required>
-                        <option value="Male">Male</option>
-                        <option value="Female">Female</option>
-                        <option value="Rather not say">Rather not say</option>
-                    </select><br><br>
-
-                    <label for="Email">Email</label>
-                    <input type="email" name="email" required value="">
-                    <br><br>
-
-                    <label for="Password">Password</label>
-                    <input type="password" name="password" required value="">
-                    <br><br>
-
-                    <label for="">Languages</label>
-                        <input type="checkbox" name="languages[]" value="English">English
-                        <input type="checkbox" name="languages[]" value="Swahili">Swahili
-                        <input type="checkbox" name="languages[]" value="Spanish">Spanish
-                    <br><br>
-                    <button type="submit" name="submit">Submit</button>
-                </form>
-        </div><br><br>
+  </form>
+  </div>
 <?php //include_once ("templates/side_bar.php"); ?>
+</body>
 
 <?php include_once ("templates/footer.php"); ?>
+</html>
+<?php
+    include 'includes/db_connect.php';
+
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){ 
+    if(isset($_POST['submit'])){
+        $name = $_POST['name'];
+        $username =$_POST['username'];
+    
+    
+    
+    try{    
+    
+      $sql = "SELECT  `Name`, `username`   FROM `elements` WHERE `Name` = '$name' AND `username` = '$username' ";
+    
+      $result = $conn->query($sql);
+    
+      $row = mysqli_num_rows($result);
+    
+      if($row){
+        header("Location: index.php");
+        exit(0);
+      }
+      else{
+        echo "<script>alert('error, tyr again')</script>";
+        header("Location: signup.php");
+      }
+    
+    }catch(msqli_sql_exception $e){
+      echo "<script>alert('error')</script>" .conn->error;
+    }
+        
+    }
+
+    
+    }    
+
+?>
+
--- templates/heading.php
+++ templates/heading.php
@@ -4,5 +4,6 @@
         <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
         <title>Welcome - Document</title>
         <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     </head>
     <body>
